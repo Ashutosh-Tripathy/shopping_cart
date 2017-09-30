cef66415d54c5962d4d80661a91456233e0fd8c2
Ashutosh Tripathy <ashutosh_tripathy@outlook.com>
2017-09-30 12:29:51 +0530

    Initial commit

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>About</h1>
+                <p>This application uses React, Redux, React rout and variety of other htlpful libraries.</p>
+            </div>
+        );
+    }
+}
+export default AboutPage;
