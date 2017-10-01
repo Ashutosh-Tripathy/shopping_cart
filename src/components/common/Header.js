8ce51bbc78c0a9b5e01c35a27f0b122ec1b384b6
Ashutosh Tripathy <ashutosh_tripathy@outlook.com>
2017-10-02 01:12:16 +0530

    Remove veiw and mocks

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -3,12 +3,7 @@ import { Link, IndexLink } from 'react-router';
 import LoadingDots from './LoadingDots';
 const Header = ({ loading }) => {
     return (
-        <nav>
-            <IndexLink to="/" activeClassName="active">Home</IndexLink>
-            {" | "}
-            <Link to="/courses" activeClassName="active">Courses</Link>
-            {" | "}
-            <Link to="/about" activeClassName="active">About</Link>
+        <nav>            
             {loading && <LoadingDots interval={100} dots={20} />}
         </nav>
     );
